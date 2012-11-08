ceb73a885e3291bd6b779c2065862abf41e17e7b
djmedina <diegurrio@gmail.com>
2012-11-07 18:19:40 -0800

    First commit

---
+++ project/appClientModule/crawl_server_project/CrawlerServer@4500.ru
@@ -0,0 +1,3 @@
+require './crawlerServer'
+#\ -p 4500
+run CRAWLERSERVER.new(4500)
---
+++ project/appClientModule/crawl_server_project/test.rb
@@ -0,0 +1,18 @@
+#Include require gems\libs
+require 'rubygems'
+require 'sinatra'
+#require 'mongoid'
+require 'haml'
+require 'json'
+
+
+#**********************************************************************
+#Navigation Routes - TODO - this may need to change for intgretion
+#Index page
+get '/' do
+  haml :index
+end
+
+get '/hi' do
+   "Hello, my friends!"
+end
